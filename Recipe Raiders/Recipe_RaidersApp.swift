9b1990d75e1077d0c941378a20d35d014dfdd0e8
Sita <sitavemuri8@gmail.com>
2023-06-27 14:13:28 -0700

    Initial Commit

---
+++ Recipe Raiders/Recipe_RaidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_RaidersApp.swift
+//  Recipe Raiders
+//
+//  Created by Sita on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_RaidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
